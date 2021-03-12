eaa620454770c61401212874ba2db1e391d0aedc
Vanya-Karalev <74813089+Vanya-Karalev@users.noreply.github.com>
2021-03-12 14:57:39 +0300

    Add files via upload

---
+++ LR 2. Numbers, Dates, Text/LR 2. Numbers/LR 2. Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static int GetAllDva(int x)
+        {
+            if (x > 1)
+                return x / 2 + GetAllDva(x / 2);
+            else
+                return 0;
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Enter integer a: ");
+            string A = Console.ReadLine();
+            if (!int.TryParse(A, out a))
+            {
+                Console.WriteLine("Invalid input!");
+            }
+            Console.Write("Enter integer b: ");
+            string B = Console.ReadLine();
+            if (!int.TryParse(B, out b)) 
+            {
+                Console.WriteLine("Invalid input!");
+                return;
+            }
+            Console.WriteLine("Output: " + (GetAllDva(b) - GetAllDva(a-1)));
+        }
+    }
+}
---
+++ LR 2. Numbers, Dates, Text/LR 2. Text/LR 2. Text/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задание №2
+using System;
+using System.Text.RegularExpressions;
+
+namespace Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string text = Console.ReadLine();
+            if (Regex.IsMatch(text, @"^[a-zA-ZА-ЯЁа-яё ]+$")) { } //Строка для поиска совпадения.
+            else 
+            {
+                Console.WriteLine("Invalid input...");
+                return;
+            }
+            string[] words = text.Split(' '); //Разбивает строку на подстроки на основе предоставленного разделителя строк.
+            Array.Reverse(words); //Изменяет последовательность элементов во всем одномерном массиве .
+            Console.Write("\nOutput: ");
+            foreach (string st in words) //Цикл foreach позволяет проходить по каждому элементу коллекции
+            {
+                Console.Write(st + " ");
+            }
+        }
+    }
+}
