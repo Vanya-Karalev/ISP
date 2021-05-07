22679355100d47074a4475fe62a3e157d3bc92b6
Vanya-Karalev <74813089+Vanya-Karalev@users.noreply.github.com>
2021-05-07 10:50:52 +0300

    Add files via upload

---
+++ lab4 task2/Dll/Class2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Dll
+{
+    public class Class2
+    {
+        static public double Tetraedr()
+        {
+            double V;
+            double r = 0;
+            while(true)
+            {
+                try
+                {
+                    r = Convert.ToDouble(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Введите число");
+                    continue;
+                }
+                if (r < 0)
+                {
+                    Console.WriteLine("Введите положительное значение");
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            V = (Math.Pow(r, 3) * Math.Sqrt(2)) / 12;
+            return V;
+        }
+    }
+}
