b2960a74507ea048016dc53386986f40bba05594
Vanya-Karalev <74813089+Vanya-Karalev@users.noreply.github.com>
2021-05-14 15:58:25 +0300

    Delete lab4.task2 directory

--- lab4.task2/lab4.task2c++dll/DefaultMath.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-
-
-extern "C" __declspec(dllexport) int Summ(int a, int b);
-extern "C" __declspec(dllexport) int Minus(int a, int b);
-extern "C" __declspec(dllexport) int Multi(int a, int b);
-extern "C" __declspec(dllexport) int Div(int a, int b);
