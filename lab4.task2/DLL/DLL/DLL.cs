08f74ab4b3bc1056e728de0b49eb776a930fd1a2
Vanya-Karalev <74813089+Vanya-Karalev@users.noreply.github.com>
2021-05-14 15:59:42 +0300

    Add files via upload

---
+++ lab4.task2/DLL/DLL/DLL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL
+{
+    unsafe public static class DLL
+    {
+        public static void Sort(int* nums, int length)
+        {
+            int temp;
+            for (int i = 0; i < length - 1; i++)
+            {
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ lab4.task2/lab4.task2/lab4.task2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using static DLL.DLL;
+
+namespace lab4.task2
+{ 
+    static class Program
+    {
+        static int CheckInt()
+        {
+            int Value;
+            while (!int.TryParse(Console.ReadLine(), out Value))
+            {
+                Console.Write("Некоректный ввод, повторите попытку: ");
+            }
+            return Value;
+        }
+        unsafe public static void Main(string[] args)
+        {
+
+            // ввод чисел
+            int[] nums = new int[20];
+            Random rands = new Random();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rands.Next() % 100;
+            }
+            for (int i = 0; i < nums.Length; i++)
+                Console.WriteLine(nums[i]);
+            // сортировка
+            Console.WriteLine("Хотите ли вы отсортировать массив ? \n1-да \nлюбое другое число-нет");
+            int FirstValue = CheckInt();
+            if (FirstValue == 1)
+            {
+                fixed (int* p = nums)
+                {
+                    Sort(p, nums.Length);
+                }
+                Console.WriteLine("Вывод отсортированного массива");
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    Console.Write($"{nums[i]} ");
+                }
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Вы вышли из программы");
+                Environment.Exit(0);
+            }
+        }
+    }
+}
